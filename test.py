ae8623ebe5831563525267fff8dc0b4b3767696b
slbryson <sidney.bryson@alcatel-lucent.com>
2014-10-18 10:34:06 -0500

    Made modifications to search the Boston Area and some comments to make sure redis is running

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ CS = 'jFH6tuBHKaV6B0Am5cGCJpydZiYbSnuz6hqPlZZMSnzvM9jOF1'
 AT = '2742390818-BYNy3F2ONHVe85nTby7UVm3JPuOn4DaGef2swip'
 ATS = 'vicUnQoWftfkLhCW7wsJV1oNmmCAQG3j1EDvIuh3ss6GQ'
 api = TwitterAPI.TwitterAPI(CK, CS, AT, ATS)
-r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
-for item in r.get_iterator():
-    print item
+#r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
+#for item in r.get_iterator():
+#    print item
 
 serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
 import json
 import urllib
-address = 'Phoenix, AZ'
+address = 'Boston, MA'
 url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
 uh = urllib.urlopen(url)
 data = uh.read()
@@ -30,7 +30,11 @@ r = api.request('statuses/filter', {'locations': bbox_strng})
 count = 0
 tws = []
 for item in r.get_iterator():
-    tws.append(item['geo']['coordinates'])
+    try:
+        tws.append(item['geo']['coordinates'])
+        print (item['geo']['coordinates'])
+    finally:
+        print "No coordinates"
     count = count + 1
     if count == 20:
         break
